284a75f1789d251569416f30c0147fd64f33b840
Багаутдинов-Ринат <tensal@bk.ru>
2025-08-06 12:42:03 +0300

    hw_30: Рефакторинг views.py на классовые представления

--- barbershop_project/users/views.py
+++ barbershop_project/users/views.py
@@ -1,34 +1,25 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 from .forms import UserRegisterForm, UserLoginForm
 
-def register_view(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Автоматический вход после регистрации
-            return redirect('/')  # Редирект на главную
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+class RegisterView(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('landing')  # Редирект на главную после регистрации
 
-def login_view(request):
-    if request.method == 'POST':
-        form = UserLoginForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('/')  # Редирект на главную
-    else:
-        form = UserLoginForm()
-    return render(request, 'users/login.html', {'form': form})
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.save()
+        login(self.request, user)  # Автоматический вход после регистрации
+        return response
 
-@login_required
-def logout_view(request):
-    logout(request)
-    return redirect('/')  # Редирект на главную после выхода
+class UserLoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True  # Перенаправлять уже авторизованных пользователей
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('landing')  # Редирект на главную после выхода
