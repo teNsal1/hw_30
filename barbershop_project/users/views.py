9694734bba7dd53f5a27a8b106fd9c984b4fcffb
Багаутдинов-Ринат <tensal@bk.ru>
2025-08-06 13:03:16 +0300

    hw_30: Доработка функции выхода из учётной записи

--- barbershop_project/users/views.py
+++ barbershop_project/users/views.py
@@ -1,25 +1,31 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.views.generic import CreateView, View
 from django.urls import reverse_lazy
 from .forms import UserRegisterForm, UserLoginForm
 
 class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('landing')  # Редирект на главную после регистрации
+    success_url = reverse_lazy('landing')
 
     def form_valid(self, form):
         response = super().form_valid(form)
         user = form.save()
-        login(self.request, user)  # Автоматический вход после регистрации
+        login(self.request, user)
         return response
 
-class UserLoginView(LoginView):
+class UserLoginView(BaseLoginView):
     form_class = UserLoginForm
     template_name = 'users/login.html'
-    redirect_authenticated_user = True  # Перенаправлять уже авторизованных пользователей
+    redirect_authenticated_user = True
 
-class UserLogoutView(LogoutView):
-    next_page = reverse_lazy('landing')  # Редирект на главную после выхода
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        return render(request, 'users/logout.html')
+    
+    def post(self, request):
+        logout(request)
+        return render(request, 'users/logout.html')
