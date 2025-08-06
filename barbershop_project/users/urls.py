412d416b28b3d14b4fa0ba30f594eb584dd6209a
Багаутдинов-Ринат <tensal@bk.ru>
2025-08-06 12:42:47 +0300

    hw_30: Обновление urls.py для использования классовых представлений

--- barbershop_project/users/urls.py
+++ barbershop_project/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import RegisterView, UserLoginView, UserLogoutView
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
